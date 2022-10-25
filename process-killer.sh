b46cd3dcee23ed6eef66dad44e3cdc0a36f345db
KostasEreksonas <k.ereksonas@gmail.com>
2022-10-25 16:34:45 +0300

    Enter a number of process to kill

--- process-killer.sh
+++ process-killer.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 read -p "Enter a process name that you want to stop: " name
+processes=$(ps -ef | grep $name) && printf "$processes\n" # Processes associated with given name
 
-process=$(ps -ef | grep $name | cut -d " " -f 6 | head -n 1); kill -9 $process && xrandr --output eDP1 --auto --output DP1 --right-of eDP1 --auto
+# Enter a number of the process to kill
+read -p "Enter number of the process you want to stop: " number
+process=$(ps -ef | grep $name | cut -d " " -f 6 | head -n $number | tail -n $number-1) && kill -9 $process
